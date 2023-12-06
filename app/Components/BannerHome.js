255641543632b22c4c679eeff3861f98a593770b
javadBoroji <javadboroji2222@gmail.com>
2023-12-06 10:19:54 +0330

    change Banner Web v2

--- app/Components/BannerHome.js
+++ app/Components/BannerHome.js
@@ -2,7 +2,6 @@ import Link from "next/link";
 function BannerHome() {
   return (
     <div className="banner-main d-flex justify-content-center position-absolut">
-      
       <div className="banner-content  d-flex align-items-center">
         <div className="container">
           <div className="row d-flex  ">
@@ -14,14 +13,18 @@ function BannerHome() {
                   تولید کننده انواع کلیشه های ژلاتینی (دیجیتال){" "}
                 </p>
                 <span className="description">
-                  شرکت رسام نقش آناهیتا در سال ۱۴۰۰ با تکیه بر دانش فنی موسسان
-                  خود تاسیس شد این شرکت با راه اندازی تعداد دو خط از ماشین آلات
-                  لیزری تولید کلیشه اقدام به تولید کلیشه های ژلاتینی لیزری نموده
-                  و با راه اندازی ۲ خط کلیشه دیجیتال با استفاده از تکنولوژی شرکت
-                  کداک قابلیت تولید این محصول با کیفیت بسیار بالا را دارد.
+                  شرکت رسام نقش آناهیتا در سال ۱۴۰۰ در کیلومتر ۲۵ جاده مخصوص
+                  کرج‌‌ تاسیس و راه انداژی شد. این مجموعه با توانایی تولید کلیشه
+                  های لیزری برای انواع صنایع تولید چاپ فلکسو توانسته است تجربه و
+                  تخصص را در کنار سرعت و کیفیت در اختیار مشتریان خود قرار دهد.
+                  محصولات ما در انواع چاپ های فلکسو از جمله Wide web , Mide web
+                  , Narrow web و چاپهای High graphics corrugated قابل مصرف است.
                 </span>
                 <button type="button" className=" btn-blue-gradient">
-                <Link href={'https://formafzar.com/RhtudQdy6L/formLogin/3'}> ثبت سفارش </Link>
+                  <Link href={"https://formafzar.com/RhtudQdy6L/formLogin/3"}>
+                    {" "}
+                    ثبت سفارش{" "}
+                  </Link>
                 </button>
               </div>
             </div>
