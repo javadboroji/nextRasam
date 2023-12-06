325b335cc8be6054e12dd6cacd6a92903b828393
javadBoroji <javadboroji2222@gmail.com>
2023-12-06 11:12:20 +0330

    change video page

--- app/Components/VideoList/VideoList.js
+++ app/Components/VideoList/VideoList.js
@@ -5,7 +5,9 @@ import useSWR from "swr";
 const fetcher = async () => {
   const res = await fetch("http://webapp.rasamflexo.ir/api/v1/Video",{cache: 'no-store' });
   if(!res.ok){
-    throw new Error('Failed to fetch data lats Article')
+
+    console.log("error");
+    
   }
   return res.json()
 };
@@ -14,21 +16,27 @@ const fetcher = async () => {
 
 async function VideoList() {
   const  data  =await fetcher();
-  return (
-    <div className="container ">
-      <div className="row  ">
-        {data?.data?.rows.map((video) => {
-          
-          return (
-            <div className="col-12 col-lg-4 mb-3 video-hover" key={video.Id}>
-              <div className="video-box"><div dangerouslySetInnerHTML={{ __html: video.embeddedUrl }} /></div>
-              <p className="video-title">{video.Title}</p>
+    return (
+      <div className="container ">
+        <div className="row  ">
+        {data?.data?.rows && data.data.rows.length > 0 ? (
+        data.data.rows.map((video) => (
+          <div className="col-12 col-lg-4 mb-3 video-hover" key={video.Id}>
+            <div className="video-box">
+              <div dangerouslySetInnerHTML={{ __html: video.embeddedUrl }} />
             </div>
-          );
-        })}
+            <p className="video-title">{video.Title}</p>
+          </div>
+        ))
+      ) : (
+        <div className="col-12">
+          <p> ویدیویی وجود ندارد!  </p>
+        </div>
+      )}
+        </div>
       </div>
-    </div>
-  );
+    );
+  
 }
 
 export default VideoList;
